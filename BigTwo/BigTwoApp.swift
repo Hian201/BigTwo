f439ec64dc6990200ac63e0ce4ad770831eb8fa9
Syuan <s.yihsuan0201@gmail.com>
2022-11-02 09:00:14 +0800

    Initial Commit

---
+++ BigTwo/BigTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigTwoApp.swift
+//  BigTwo
+//
+//  Created by So í-hian on 2022/11/2.
+//
+
+import SwiftUI
+
+@main
+struct BigTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
